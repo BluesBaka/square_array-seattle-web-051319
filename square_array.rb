06bdc70a6f353af7620d18c657f7ef5d86e64b45
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 01:36:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-   puts array.each{|x|; x**x}
+   puts array.each{|x| x**x}
    array
 end
 
 def collected_square_array(array)
-   puts array.collect{|x|; x+x}
+   puts array.collect{|x| x+x}
    array
 end
