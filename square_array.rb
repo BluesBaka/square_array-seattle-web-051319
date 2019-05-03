f5de0db7aaf407a4004d96af07e783492e1830bf
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 01:48:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
    new_array = []
-   array.each{|x| new_array.push x**x}
+   array.each{|x| new_array.push x*x}
     new_array
 end
 
 def collected_square_array(array)
-   puts array.collect{|x| x+x}
+   puts array.collect{|x| x*x}
    array
 end
